22a99d2da5528cd4e26c376856aadc50da1a721f
Immo Landwerth <immo@landwerth.net>
2017-03-26 00:55:28 -0700

    Make build discover the correct version of MSBuild

--- build.cmd
+++ build.cmd
@@ -1,7 +1,21 @@
 @echo off
 
-set MSBuild="%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
+for /d %%F in ("%ProgramFiles(x86)%\Microsoft Visual Studio\2017\*") DO (
+ set MSBuild="%%F\MSBuild\15.0\Bin\MSBuild.exe"
+ goto validate
+)
+
+:validate
+
+if exist %MSBuild% goto build
+echo ERROR: You need Visual Studio 2017 to build.
+exit
+
+:build
+
 set OutDir="%~dp0bin"
 
 if not exist %OutDir% mkdir %OutDir%
-%MSBuild% /nologo /m /v:m /nr:false /flp:logfile=bin\msbuild.log;verbosity=normal /t:Restore /t:Build /p:OutDir=%OutDir% %* 
+%MSBuild% /nologo /m /v:m /nr:false /flp:logfile=bin\msbuild.log;verbosity=normal /t:Restore /t:Build /p:OutDir=%OutDir% %*
+exit
+
