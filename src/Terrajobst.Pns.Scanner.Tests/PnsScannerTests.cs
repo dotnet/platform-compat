2ff6fc5006bd6cefdfd8328decb3edb841002e1e
Immo Landwerth <immo@landwerth.net>
2017-04-01 14:17:21 -0700

    Extract infrastructure into abstract base class

--- src/Terrajobst.Pns.Scanner.Tests/PnsScannerTests.cs
+++ src/Terrajobst.Pns.Scanner.Tests/PnsScannerTests.cs
@@ -1,82 +1,10 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using Microsoft.Cci;
-using Microsoft.Cci.Extensions;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
+﻿using Terrajobst.Pns.Scanner.Tests.Helpers;
 using Xunit;
 
 namespace Terrajobst.Pns.Scanner.Tests
 {
-    public partial class PnsScannerTests
+    public partial class PnsScannerTests : PnsTests
     {
-        private static readonly CSharpCompilation CompilationTemplate = CSharpCompilation.Create(
-            "dummy.dll",
-            references: new[] { MetadataReference.CreateFromFile(typeof(object).Assembly.Location) },
-            options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary)
-        );
-
-        private static void AssertMatch(string source, string matches)
-        {
-            using (var host = new HostEnvironment())
-            {
-                var expectedDocIds = ParseLines(matches);
-                var assembly = CreateAssembly(host, source);
-                var actualDocIds = GetResults(assembly).Select(r => r.docId);
-
-                Assert.Equal(expectedDocIds, actualDocIds);
-            }
-        }
-
-        private static void AssertNoMatch(string source)
-        {
-            AssertMatch(source, string.Empty);
-        }
-
-        private static IAssembly CreateAssembly(HostEnvironment host, string text)
-        {
-            var syntaxTree = CSharpSyntaxTree.ParseText(text);
-            var compilation = CompilationTemplate.AddSyntaxTrees(syntaxTree);
-
-            using (var memoryStream = new MemoryStream())
-            {
-                var result = compilation.Emit(memoryStream);
-                Assert.Empty(result.Diagnostics);
-
-                memoryStream.Position = 0;
-
-                return host.LoadAssemblyFrom(compilation.AssemblyName, memoryStream);
-            }
-        }
-
-        private static IEnumerable<(string docId, PnsResult result)> GetResults(IAssembly assembly)
-        {
-            var results = new List<(string docId, PnsResult result)>();
-            var handler = new DelegatedPnsReporter((r, m) =>
-            {
-                if (r.Throws)
-                    results.Add((m.DocId(), r));
-            });
-            var scanner = new PnsScanner(handler);
-            scanner.AnalyzeAssembly(assembly);
-            return results;
-        }
-
-        private static IEnumerable<string> ParseLines(string text)
-        {
-            using (var reader = new StringReader(text))
-            {
-                string line;
-                while ((line = reader.ReadLine()) != null)
-                {
-                    line = line.Trim();
-                    if (line.Length > 0)
-                        yield return line.Trim();
-                }
-            }
-        }
-
         [Fact]
         public void PnsScanner_DoesNotDetect_Privates()
         {
