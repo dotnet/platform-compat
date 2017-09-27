ee50d23b9d95e596dba955265f8c39dec3cc923a
Immo Landwerth <immol@microsoft.com>
2017-09-27 14:25:51 -0700

    Allow build.cmd to be called outside of a VS command prompt

--- build.cmd
+++ build.cmd
@@ -1,6 +1,23 @@
 @echo off
+setlocal
+
+:: Use VSINSTALLDIR if present
+
+if not "%VSINSTALLDIR%" == "" goto setmsbuild
+
+:: Use vswhere to find an install of VS 2017
+
+set VSWHERE="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe"
+if not exist %VSWHERE% goto error
+for /f "tokens=*" %%i in ('%VSWHERE% -property installationPath') do set VSINSTALLDIR=%%i
+
+:setmsbuild
 
 set MSBuild="%VSINSTALLDIR%\MSBuild\15.0\Bin\MSBuild.exe"
+if exist %MSBuild% goto build
+
+:error
+
 if exist %MSBuild% goto build
 echo ERROR: You need Visual Studio 2017 to build.
 exit /B -1
